3663369962d0fcbd5b2a8a075cf3758f6c733eea
mstars_lab3_15 <mstars_lab3_15@mstars-lab3-15s-iMac.local>
2022-08-23 12:53:51 +0800

    feat:front-edn

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import mongoose, { ConnectOptions } from "mongoose";
 import userRouter from "./routes/userRouter";
+import cors from "cors";
 dotenv.config();
 const app: Express = express();
 const PORT = process.env.PORT;
@@ -14,6 +15,12 @@ mongoose.connect(DB_CONNECTION).then(() => {
   });
 });
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "DELETE", "PUT", "POST"],
+  })
+);
 app.use("/users", userRouter);
 // app.listen(PORT, () => {
 //   console.log(`Server is running ${PORT}`);
---
+++ dating-app/pages/register.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+const register = (props: Props) => {
+  return <div>register</div>;
+};
